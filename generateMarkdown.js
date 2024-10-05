f133656bee32cfbdcdf4d2b8618e786e49369b5e
elisabethtracy <elisabethtracy@gmail.com>
2024-10-05 16:57:57 -0500

    created renderLicenseSection function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -34,10 +34,17 @@ if (!license) {
 };
 };
 
-
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+function renderLicenseSection(license) {
+  const badge = renderLicenseBadge(license);
+  const link = renderLicenseLink(license);
+  if (!license) {
+    return '';
+  } else {
+    return `${badge}\n${link}`;
+  };
+};
 
 
 // TODO: Create a function to generate markdown for README
